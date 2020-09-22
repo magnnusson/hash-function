2d7384cacc5b571b281fd9c34bf8a071db758f10
shogun87 <45832125+shogun87@users.noreply.github.com>
2020-09-22 15:41:25 -0600

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -5,4 +5,8 @@ Created on Tue Sep 22 15:35:11 2020
 
 @author: alanhandukic, daviddarling
 """
-
+def main():
+  """ This is the main function to run our program."""
+  # Make our variables
+  constant_list = []
+  
