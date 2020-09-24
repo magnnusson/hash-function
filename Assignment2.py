b6ec72514a80aca65669e7dfa54e9ff8061b224a
Alen Handukic <43865831+magnnusson@users.noreply.github.com>
2020-09-24 15:36:40 -0600

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,50 +1,56 @@
 import math
 
-prime_list = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,
-              139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,
-              281,283,293,307,311] # a list of the first 64 prime numbers
+prime_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
+              107, 109, 113, 127, 131, 137,
+              139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251,
+              257, 263, 269, 271, 277,
+              281, 283, 293, 307, 311]  # a list of the first 64 prime numbers
 
-NUM_BITS = 16 # Constant number of bits that we want
+NUM_BITS = 16  # Constant number of bits that we want
+
 
 def process_primes(prime_list):
-  cube_roots = [] #this will hold our cube roots
-  constants = []
-  for index, prime in enumerate(prime_list):
-    prime = prime**(1/3)
-    cube_roots.append(prime) # calculate and hold all cube roots in another list
+    cube_roots = []  # this will hold our cube roots
+    constants = []
+    for index, prime in enumerate(prime_list):
+        prime = prime ** (1 / 3)
+        cube_roots.append(prime)  # calculate and hold all cube roots in another list
 
-  for index, cubes in enumerate(cube_roots):
-    cubes = math.modf(cubes) # use modf to get the fractional part of the cube root
-    fraction = cubes[0]
-    fraction = fraction * (2**16) # multiply the fractional part by 2^16, truncate, and then convert to binary
-    fraction = int(fraction)
-    fraction = bin(fraction)[2:]
-    constants.append(fraction) # we have our list of constants
+    for index, cubes in enumerate(cube_roots):
+        cubes = math.modf(cubes)  # use modf to get the fractional part of the cube root
+        fraction = cubes[0]
+        fraction = fraction * (2 ** 16)  # multiply the fractional part by 2^16, truncate, and then convert to binary
+        fraction = int(fraction)
+        fraction = bin(fraction)[2:]
+        constants.append(fraction)  # we have our list of constants
 
-  return constants
+    return constants
+
 
 def parse_user_input():
-  new_string = [] # we will hold the final values here
-  user_string = input("Enter a string: ")
-  user_string = [letter for letter in user_string] # split the user's input into separate characters in a list
+    new_string = []  # we will hold the final values here
+    user_string = input("Enter a string: ")
+    user_string = [letter for letter in user_string]  # split the user's input into separate characters in a list
 
-  for index, letter in enumerate(user_string):
-    ascii_val = ord(letter)
-    ascii_val = bin(ascii_val)[2:]
-    new_string.append(ascii_val)
-    binary_val = ''.join(new_string)
+    for index, letter in enumerate(user_string):
+        ascii_val = ord(letter)
+        ascii_val = bin(ascii_val)[2:]
+        new_string.append(ascii_val)
+        binary_val = ''.join(new_string)
 
-  return binary_val
+    return binary_val
+
 
 def pad_message(user_string):
-  user_string = str(user_string) # first concatenate a 1 to the message
-  user_string += '1'
+    user_string = str(user_string)  # first concatenate a 1 to the message
+    user_string += '1'
 
-  while len(user_string) <= 512: # add 448 0s to the message until we have a length of 512
-    user_string += '0'
+    while len(user_string) <= 512:  # add 448 0s to the message until we have a length of 512
+        user_string += '0'
 
-  padded_string = int(user_string) # return the padded string
-  return padded_string
+    padded_string = int(user_string)  # return the padded string
+    return padded_string
+
 
 def split_block(msg_block):
     divided_list = []
@@ -62,22 +68,22 @@ def split_block(msg_block):
 
 
 def create_message_schedule(divided_string):
-  while len(divided_string) <= 63: # stop once we reach 64 words
-    length_of_string = len(divided_string) # updating the value of the length
-    val1 = sigma1(divided_string[length_of_string-2]) # first we perform an upper-case sigma 1 rotation to this element
-    val2 = divided_string[length_of_string-7] # we just retrieve this element
-    val3 = sigma0(divided_string[length_of_string-31]) # we perform a lower-case sigma 0 rotation to this element
-    val4 = divided_string[length_of_string-32] # we just retrieve this element
+    while len(divided_string) <= 63:  # stop once we reach 64 words
+        length_of_string = len(divided_string)  # updating the value of the length
+        val1 = sigma1(divided_string[length_of_string - 2])  # first we perform an upper-case sigma 1 rotation to this element
+        val2 = divided_string[length_of_string - 7]  # we just retrieve this element
+        val3 = sigma0(divided_string[length_of_string - 31])  # we perform a lower-case sigma 0 rotation to this element
+        val4 = divided_string[length_of_string - 32]  # we just retrieve this element
 
-    sum = int(val1, 2) + int(val2, 2) + int(val3, 2) + int(val4, 2) # add the binary numbers together
-    bin_sum = bin(sum)[2:]
-    if len(bin_sum) > NUM_BITS:
-      bin_sum = bin_sum[len(bin_sum) - NUM_BITS:] # truncate to 16 bits if needed
+        sum = int(val1, 2) + int(val2, 2) + int(val3, 2) + int(val4, 2)  # add the binary numbers together
+        bin_sum = bin(sum)[2:]
+        if len(bin_sum) > NUM_BITS:
+            bin_sum = bin_sum[len(bin_sum) - NUM_BITS:]  # truncate to 16 bits if needed
 
+        divided_string.append(bin_sum)  # append the binary sum to the end of the list
 
-    divided_string.append(bin_sum) # append the binary sum to the end of the list
+    return divided_string
 
-  return divided_string
 
 def sigma0(curr_word):
     word_val = int(curr_word, 2)  # This will convert binary string into number
@@ -90,125 +96,141 @@ def sigma0(curr_word):
     bin_num = bin(temp1)[2:]
 
     if len(bin_num) < NUM_BITS:
-      temp_str = '0' * (NUM_BITS - len(bin_num))
-      temp_str += bin_num
-      return temp_str
+        temp_str = '0' * (NUM_BITS - len(bin_num))
+        temp_str += bin_num
+        return temp_str
+    else:
+        return bin_num
+
+
+def sigma1(curr_word):
+    word_val = int(curr_word, 2)
+    temp1 = right_rotate(word_val, 13, NUM_BITS)  # Should be 17 however we are using 16 bit words
+    temp2 = right_rotate(word_val, 15, NUM_BITS)  # Should be 19 however we are using 16 bit words
+    temp3 = word_val >> 10  # Shift word_val to the right 3
+    temp1 = temp1 ^ temp2  # XOR temp1 with temp2
+    temp1 = temp1 ^ temp3  # XOR new temp1 with temp3
+
+    bin_num = bin(temp1)[2:]
+
+    if len(bin_num) < NUM_BITS:
+        temp_str = '0' * (NUM_BITS - len(bin_num))
+        temp_str += bin_num
+        return temp_str
+    else:
+        return bin_num
+
+
+def upper_sigma0(curr_word):
+    word_val = int(curr_word, 2)
+    temp1 = right_rotate(word_val, 2, NUM_BITS)
+    temp2 = right_rotate(word_val, 13, NUM_BITS)
+    temp3 = right_rotate(word_val, 14, NUM_BITS)  # Should be 22 however we are using 16 bit words
+    temp1 = temp1 ^ temp2  # XOR temp1 with temp2
+    temp1 = temp1 ^ temp3  # XOR new temp1 with temp3
+
+    bin_num = bin(temp1)[2:]
+    if len(bin_num) < NUM_BITS:
+        temp_str = '0' * (NUM_BITS - len(bin_num))
+        temp_str += bin_num
+        return temp_str
     else:
       return bin_num
 
 
-def sigma1(curr_word):
-  word_val = int(curr_word, 2)
-  temp1 = right_rotate(word_val, 13, NUM_BITS)  # Should be 17 however we are using 16 bit words
-  temp2 = right_rotate(word_val, 15, NUM_BITS)  # Should be 19 however we are using 16 bit words
-  temp3 = word_val >> 10  # Shift word_val to the right 3
-  temp1 = temp1 ^ temp2  # XOR temp1 with temp2
-  temp1 = temp1 ^ temp3  # XOR new temp1 with temp3
-
-  bin_num = bin(temp1)[2:]
-
-  if len(bin_num) < NUM_BITS:
-    temp_str = '0' * (NUM_BITS - len(bin_num))
-    temp_str += bin_num
-    return temp_str
-  else:
-    return bin_num
-
-def upper_sigma0(curr_word):
-    word_val = int(curr_word, 2)
-    temp1 = right_rotate(word_val, 2, NUM_BITS) 
-    temp2 = right_rotate(word_val, 13, NUM_BITS) 
-    temp3 = right_rotate(word_val, 14, NUM_BITS) # Should be 22 however we are using 16 bit words
-    temp1 = temp1 ^ temp2 # XOR temp1 with temp2
-    temp1 = temp1 ^ temp3 # XOR new temp1 with temp3
-    
-    bin_num = bin(temp1)[2:]
-    if len(bin_num) < NUM_BITS:
-        temp_str = '0' * (NUM_BITS - len(bin_num))
-        temp_str += bin_num
-            
-    return temp_str
 
 def upper_sigma1(curr_word):
     word_val = int(curr_word, 2)
-    temp1 = right_rotate(word_val, 6, NUM_BITS) 
-    temp2 = right_rotate(word_val, 11, NUM_BITS) 
-    temp3 = right_rotate(word_val, 15, NUM_BITS) # Should be 25 however we are using 16 bit words 
-    temp1 = temp1 ^ temp2 # XOR temp1 with temp2
-    temp1 = temp1 ^ temp3 # XOR new temp1 with temp3
-    
+    temp1 = right_rotate(word_val, 6, NUM_BITS)
+    temp2 = right_rotate(word_val, 11, NUM_BITS)
+    temp3 = right_rotate(word_val, 15, NUM_BITS)  # Should be 25 however we are using 16 bit words
+    temp1 = temp1 ^ temp2  # XOR temp1 with temp2
+    temp1 = temp1 ^ temp3  # XOR new temp1 with temp3
+
     bin_num = bin(temp1)[2:]
     if len(bin_num) < NUM_BITS:
         temp_str = '0' * (NUM_BITS - len(bin_num))
         temp_str += bin_num
-            
-    return temp_str
+        return temp_str
+    else:
+      return bin_num
+
+
 
 def intialize_state_registers():
-  state_registers = []
-  square_roots = []
-  for index, prime in enumerate(prime_list):
-    if index <= 7: # we only want the first 8 primes
-      prime = math.sqrt(prime)
-      square_roots.append(prime)
+    state_registers = []
+    square_roots = []
+    for index, prime in enumerate(prime_list):
+        if index <= 7:  # we only want the first 8 primes
+            prime = math.sqrt(prime)
+            square_roots.append(prime)
 
-  for index, square in enumerate(square_roots):
-    fraction = math.modf(square) # use modf to get the irrational part of the number
-    fraction = fraction[0]
-    fraction = fraction * (2 ** 16)  # multiply the fractional part by 2^16, truncate, and then convert to binary
-    fraction = int(fraction)
-    fraction = bin(fraction)[2:]
-    state_registers.append(fraction)  # we have our 8 state registers with initial hash values
+    for index, square in enumerate(square_roots):
+        fraction = math.modf(square)  # use modf to get the irrational part of the number
+        fraction = fraction[0]
+        fraction = fraction * (2 ** 16)  # multiply the fractional part by 2^16, truncate, and then convert to binary
+        fraction = int(fraction)
+        fraction = bin(fraction)[2:]
+        state_registers.append(fraction)  # we have our 8 state registers with initial hash values
 
-  return state_registers
+    return state_registers
 
 
-def compression(message_schedule, constants_list, state_registers):
-  for word, constant in zip(message_schedule, constants_list): # parallel iteration of the schedule and constants
-    val1 = uppercase_sigma_1(state_registers[4]) # take the uppercase sigma 1 rotation of register 'e', index 4
-    val2 = choice(state_registers[4], state_registers[5], state_registers[6]) # choosing 1 or 0 from either of these registers to create the value
-    val3 = state_registers[7] # the 'h' register value
-    val4 = constant # the current constant value
-    val5 = word # the current word value from the schedule
-    temp_word_1 = val1 + val2 + val3 + val4 + val5 # we have our first temp word
+'''def compression(message_schedule, constants_list, state_registers):
+    for word, constant in zip(message_schedule, constants_list):  # parallel iteration of the schedule and constants
+      val1 = upper_sigma1(state_registers[4])  # take the uppercase sigma 1 rotation of register 'e', index 4
+      val2 = choice(state_registers[4], state_registers[5], state_registers[6])  # choosing 1 or 0 from either of these registers to create the value
+      val3 = state_registers[7]  # the 'h' register value
+      val4 = constant  # the current constant value
+      val5 = word  # the current word value from the schedule
 
-    val6 = uppercase_sigma_0(state_register[0]) # take the uppercase sigma 0 rotation of register 'a', index 0
-    val7 = majority(state_registers[0], state_registers[1], state_registers[2]) # find max value between registers a, b, c
-    temp_word_2: val6 + val7 # we have our second temp word
+      sum1 = int(val1, 2) + int(val2, 2) + int(val3, 2) + int(val4, 2) + int(val5, 2) # add the binary numbers together
+      temp_word_1 = bin(sum1)[2:]
+      if len(temp_word_1) > NUM_BITS:
+        temp_word_1 = temp_word_1[len(temp_word_1) - NUM_BITS:]  # truncate to 16 bits if needed
+
+      val6 = upper_sigma0(state_registers[0])  # take the uppercase sigma 0 rotation of register 'a', index 0
+      val7 = majority(state_registers[0], state_registers[1], state_registers[2])  # find max value between registers a, b, c
+      temp_word_2: val6 + val7  # we have our second temp word
+
+      sum2 = int(val6, 2) + int(val7, 2)  # add the binary numbers together
+      temp_word_2 = bin(sum2)[2:]
+      if len(temp_word_2) > NUM_BITS:
+        temp_word_2 = temp_word_2[len(temp_word_2) - NUM_BITS:]  # truncate to 16 bits if needed'''
 
 
 
 
 def right_rotate(num, rot, num_bit):
-    return (num >> rot)|(num << (num_bit - rot)) & 0xFFFF
-
+    return (num >> rot) | (num << (num_bit - rot)) & 0xFFFF
 
 
 def main():
-  """This is the main function to run our program."""
-  constants_list = process_primes(prime_list) # This list will hold our constants that we calculate at the beginning
+    """This is the main function to run our program."""
+    constants_list = process_primes(prime_list)  # This list will hold our constants that we calculate at the beginning
 
-  user_string = parse_user_input() # This list will hold the user string's characters, in ASCII, in binary
-  len_message_in_binary = len(str(user_string))
-  padded_string = pad_message(user_string)
-  divided_string = split_block(padded_string)
-  message_schedule = create_message_schedule(divided_string)
-  print(message_schedule)
-  state_registers = intialize_state_registers() # we'll now need the state registers intialized using the prime numbers
-  compression(message_schedule, constants_list, state_registers) # compress the words of the schedule into the registers
+    user_string = parse_user_input()  # This list will hold the user string's characters, in ASCII, in binary
+    len_message_in_binary = len(str(user_string))
+    padded_string = pad_message(user_string)
+    divided_string = split_block(padded_string)
+    message_schedule = create_message_schedule(divided_string)
+    print(message_schedule)
+    state_registers = intialize_state_registers()  # we'll now need the state registers intialized using the prime numbers
+    #compression(message_schedule, constants_list, state_registers)  # compress the words of the schedule into the registers
 
     # This list will hold registers a-h for the scheduling portion
     #   of the algorithm
-   # schedule = []
+    # schedule = []
     # This list will hold the newly calculated a-h that we will add
     #   add to the old schedule
-   # schedule_new = []
+    # schedule_new = []
     # Temp 1 variable to hold the result of the temp 1 calculation
-   # t1 = 0
+    # t1 = 0
     # Temp 2 variable
-   # t2 = 0
+    # t2 = 0
     # List to hold the output from our encryption algorithm
-   # encrypt_result = []
+    # encrypt_result = []
+
 
 if __name__ == "__main__":
-  main()
+    main()
