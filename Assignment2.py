aa57e97d1c5c61a2ac3e73d7d579a8680a93c35d
Alen Handukic <43865831+magnnusson@users.noreply.github.com>
2020-09-23 18:25:53 -0600

    Update Assignment2.py
    Added Step 3 - padding the message

--- Assignment2.py
+++ Assignment2.py
@@ -22,7 +22,7 @@ def process_primes(prime_list):
   return constants
 
 def parse_user_input():
-  new_string = [] # we will hold the final values here and return the list
+  new_string = [] # we will hold the final values here
   user_string = input("Enter a string: ")
   user_string = [letter for letter in user_string] # split the user's input into separate characters in a list
 
@@ -30,33 +30,40 @@ def parse_user_input():
     ascii_val = ord(letter)
     ascii_val = bin(ascii_val)[2:]
     new_string.append(ascii_val)
+    binary_val = ''.join(new_string)
 
-  return new_string
+  return binary_val
 
-def right_rotate(num, rot, num_bit):
-    return (num >> rot)|(num << (num_bit - rot)) & 0xFFFF
+def pad_message(user_string):
+  user_string = str(user_string) # first concatenate a 1 to the message
+  user_string += '1'
+
+  while len(user_string) <= 447: # add 0s to the message until we have a length of 448
+    user_string += '0'
+
+  padded_string = int(user_string) # return the padded string
+  return padded_string
 
 def main():
   """This is the main function to run our program."""
-  # Constant number of bits that we want
-  NUM_BITS = 16
   constants_list = process_primes(prime_list) # This list will hold our constants that we calculate at the beginning
-  
+
   user_string = parse_user_input() # This list will hold the user string's characters, in ASCII, in binary
-  # This list will hold registers a-h for the scheduling portion
-  #   of the algorithm
-  schedule = []
-  # This list will hold the newly calculated a-h that we will add
-  #   add to the old schedule
-  schedule_new = []
-  # Temp 1 variable to hold the result of the temp 1 calculation
-  t1 = 0
-  # Temp 2 variable
-  t2 = 0
-  # List to hold the output from our encryption algorithm
-  encrypt_result = []
+  padded_string = pad_message(user_string)
+
+    # This list will hold registers a-h for the scheduling portion
+    #   of the algorithm
+   # schedule = []
+    # This list will hold the newly calculated a-h that we will add
+    #   add to the old schedule
+   # schedule_new = []
+    # Temp 1 variable to hold the result of the temp 1 calculation
+   # t1 = 0
+    # Temp 2 variable
+   # t2 = 0
+    # List to hold the output from our encryption algorithm
+   # encrypt_result = []
 
 if __name__ == "__main__":
   main()
-
 
