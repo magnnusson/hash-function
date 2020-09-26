136dae9f090c44cb54c75ee46da06e576f3bfa4a
shogun87 <45832125+shogun87@users.noreply.github.com>
2020-09-25 20:35:04 -0600

    Update Assignment2BruteForce.py

--- Assignment2BruteForce.py
+++ Assignment2BruteForce.py
@@ -1,5 +1,6 @@
 import math
 from collections import deque
+import sys
 
 prime_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
               107, 109, 113, 127, 131, 137,
@@ -20,7 +21,7 @@ def process_primes(prime_list):
     for index, cubes in enumerate(cube_roots):
         cubes = math.modf(cubes)  # use modf to get the fractional part of the cube root
         fraction = cubes[0]
-        fraction = fraction * (2 ** 16)  # multiply the fractional part by 2^16, truncate, and then convert to binary
+        fraction = fraction * (2 ** 8)  # multiply the fractional part by 2^16, truncate, and then convert to binary
         fraction = int(fraction)
         fraction = bin(fraction)[2:]
         constants.append(fraction)  # we have our list of constants
@@ -33,7 +34,7 @@ def parse_word(word):
     temp_str = ''
     word = [letter for letter in word]  # split the user's input into separate characters in a list
 
-    for index, letter in enumerate(word):
+    for index, letter in enumerate(word): # Convert all letters to Ascii
         ascii_val = ord(letter)
         ascii_val = bin(ascii_val)[2:]
         if len(ascii_val) < (NUM_BITS // 2):
@@ -49,7 +50,7 @@ def pad_message(user_string):
     user_string = str(user_string)  # first concatenate a 1 to the message
     user_string += '1'
 
-    while len(user_string) <= 512:  # add 448 0s to the message until we have a length of 512
+    while len(user_string) <= 512:  # add 0s to the message until we have a length of 512
         user_string += '0'
     
     # David - Commented this out, thinking we should just leave as string
@@ -61,10 +62,10 @@ def pad_message(user_string):
 def split_block(msg_block):
     divided_list = []
     curr_string = ''
-    for i, num in enumerate(str(msg_block)):
+    for i, num in enumerate(str(msg_block)): # Divide the msg_block into 64, 16 bit word elements
         if i < 16:
             curr_string += (str(num))
-        elif i % 16 == 0:
+        elif i % 16 == 0: # If i is greater than NUM_BITS (16) then start a new element
             divided_list.append(curr_string)
             curr_string = ''
             curr_string += (str(num))
@@ -105,7 +106,7 @@ def sigma0(curr_word):
 
     bin_num = bin(temp1)[2:]
 
-    if len(bin_num) < NUM_BITS:
+    if len(bin_num) < NUM_BITS: # Make sure we have length of NUM_BITS (16), if not insert 0s
         temp_str = '0' * (NUM_BITS - len(bin_num))
         temp_str += bin_num
         return temp_str
@@ -215,7 +216,7 @@ def majority(a, b, c):
     temp_list = []
     temp_str = ''
 
-    for index in range(0, len(a)):
+    for index in range(0, len(a)): # We will take the most frequent bit value among 3 words and add to string
       temp_list.append(a[index])
       temp_list.append(b[index])
       temp_list.append(c[index])
@@ -291,33 +292,16 @@ def compression(message_schedule, constants_list, state_registers):
     return state_registers
 
 
-def get_digest(bin_list):
-    hex_val = ''
+def get_digest(bin_list): # This function will return the 4 least significant bites from the registers
     digest = ''
     for i in bin_list:
-        hex_val = hex(int(i, 2))[2:]
-        if len(hex_val) != NUM_BITS // 4:
-            temp = '0' * ((NUM_BITS // 4) - len(hex_val))
-            temp += hex_val
-            digest += temp
-        else:
-            digest += hex_val
+        digest += i[12:] # Append to digest string
+
     return digest
 
 
-def right_rotate(num, rot, num_bit):
+def right_rotate(num, rot, num_bit): # Function to right rotate a bit string
     return (num >> rot) | (num << (num_bit - rot)) & 0xFFFF
-
-def read_file(filename):
-    word_list = []
-    temp_word = ''
-    with open(filename) as fp:
-        for word in fp:
-            temp_word = word.replace(" ", "")
-            temp_word = temp_word.replace("\n", "")
-            word_list.append(temp_word)
-    
-    return word_list
 
 
 def main():
@@ -335,30 +319,20 @@ def main():
                 curr_word = (str(chr(letter1)) + str(chr(letter2)) + str(chr(letter3)))
                 input_string = parse_word(curr_word)
                 len_message_in_binary = len(str(input_string))
-                padded_string = pad_message(input_string)
-                divided_string = split_block(padded_string)
+                padded_string = pad_message(input_string) # Pad the user string with mostly 0s
+                divided_string = split_block(padded_string) # Divide the string into 64 16 bit words
                 message_schedule = create_message_schedule(divided_string)
                 state_registers = intialize_state_registers()  # we'll now need the state registers intialized using the prime numbers
                 final_state_registers = compression(message_schedule, constants_list, state_registers)  # compress the words of the schedule into the registers
-                digest = get_digest(final_state_registers)
-                if digest in digest_list:
-                    print("There was a similar hash of {}".format(digest))
-                    
+                digest = get_digest(final_state_registers) # Get the digest output
+                if digest in digest_list: # See if the digest matches any other digest
+                    print("There was a similar hash of {} from the input word: {}".format(digest, curr_word))
+                    for i in range(len(word_digest_list)): # Find the matching digest and print results
+                        if digest == word_digest_list[i][1]:
+                            print("This digest matches the input: {} with digest: {}".format(word_digest_list[i][0], word_digest_list[i][1]))
+                            sys.exit("Collision Occured") # Exit with message
                 word_digest_list.append([curr_word, digest])
                 digest_list.append(digest)
-
-    # This list will hold registers a-h for the scheduling portion
-    #   of the algorithm
-    # schedule = []
-    # This list will hold the newly calculated a-h that we will add
-    #   add to the old schedule
-    # schedule_new = []
-    # Temp 1 variable to hold the result of the temp 1 calculation
-    # t1 = 0
-    # Temp 2 variable
-    # t2 = 0
-    # List to hold the output from our encryption algorithm
-    # encrypt_result = []
 
 
 if __name__ == "__main__":
